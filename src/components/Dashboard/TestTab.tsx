0e9c678e1d9e98844fb28d463393231ddb9c5d0c
BarysShauchuk <borshevchukn@gmail.com>
2023-10-29 18:57:00 +0200

    Update Test Result table

--- src/components/Dashboard/TestTab.tsx
+++ src/components/Dashboard/TestTab.tsx
@@ -1,10 +1,11 @@
-import { Breadcrumb, Button } from "react-bootstrap";
+import { Alert, Breadcrumb, Button, Col, Row, ToggleButtonGroup, ToggleButton } from "react-bootstrap";
 import AnswersGrid from "./AnswersGrid";
 import { useAppDispatch } from "../../app/hooks";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import MessageModal from "../Modals/MessageModal";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import RightSideMenu from "../RightSideMenu";
 
 const TestTab = () => {
   const dispatch = useAppDispatch();
@@ -26,46 +27,87 @@ const TestTab = () => {
   return (
     <>
       <MessageModal />
-      <div style={{ marginTop: "10px" }}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginBottom: 5,
-          }}
-        >
-          <h5 style={{ fontWeight: "bold", textAlign: "left", margin: 0 }}>
-            {main.currentTestDraft && main.currentTestDraft.title}
-            {auth.userRole === 0 && (
-              <i
-                style={{ marginLeft: 10 }}
-                className="bi bi-gear"
-                onClick={() => navigate("settings")}
-              ></i>
-            )}
-          </h5>
-          <div style={{ marginLeft: "auto" }}>
-            {auth.userRole === 0 && (
-              <Button size="sm" onClick={onEditTestClick}>
-                <i className="bi bi-pencil-square"></i> Edit test
-              </Button>
-            )}
+      <Row>
+        <Col className="col-10">
+        <div style={{ marginTop: "10px" }}>
+
+          {/* <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: 5,
+            }}
+          >
+            <h5 style={{ fontWeight: "bold", textAlign: "left", margin: 0 }}>
+              {main.currentTestDraft && main.currentTestDraft.title}
+              {auth.userRole === 0 && (
+                <i
+                  style={{ marginLeft: 10 }}
+                  className="bi bi-gear"
+                  onClick={() => navigate("settings")}
+                ></i>
+              )}
+            </h5>
+            <div style={{ marginLeft: "auto" }}>
+              {auth.userRole === 0 && (
+                <Button size="sm" onClick={onEditTestClick}>
+                  <i className="bi bi-pencil-square"></i> Edit test
+                </Button>
+              )}
+            </div>
           </div>
+          <Breadcrumb style={{ fontSize: 14 }}>
+            <Breadcrumb.Item onClick={() => navigate("/")}>
+              Dashboard
+            </Breadcrumb.Item>
+            <Breadcrumb.Item onClick={() => navigate(-1)}>
+              {selectedSubject && selectedSubject.title}
+            </Breadcrumb.Item>
+            <Breadcrumb.Item active>
+            {main.currentTestDraft && main.currentTestDraft.title}
+            </Breadcrumb.Item>
+          </Breadcrumb> */}
+          <AnswersGrid />
         </div>
-        <Breadcrumb style={{ fontSize: 14 }}>
-          <Breadcrumb.Item onClick={() => navigate("/")}>
-            Dashboard
-          </Breadcrumb.Item>
-          <Breadcrumb.Item onClick={() => navigate(-1)}>
-            {selectedSubject && selectedSubject.title}
-          </Breadcrumb.Item>
-          <Breadcrumb.Item active>
-          {main.currentTestDraft && main.currentTestDraft.title}
-          </Breadcrumb.Item>
-        </Breadcrumb>
-        <AnswersGrid />
-      </div>
+        </Col>
+        <Col className="col-2 px-0">
+          <RightSideMenu title="Test" bottomControls={
+            <Alert variant={"primary"} className="small p-2">
+              <i className="bi bi-info-circle me-2"></i>
+              Tip: double click on a row to open the attempt
+              
+              <div className="d-flex justify-content-end mt-2">
+                <Button variant="outline-primary" size="sm" className="me-2 flex-grow-1">
+                  Hide tips
+                </Button>
+                <Button size="sm">
+                  Ok
+                </Button>
+              </div>
+            </Alert>
+          }>
+            <div className="d-flex flex-column px-1">
+              <Button onClick={() => navigate("settings")} variant="light" className="mb-1 text-start border-0">
+                <i className="bi bi-sliders me-2"></i>
+                Parameters
+                </Button>
+              <Button variant="light" className="mb-1 text-start border-0">
+                <i className="bi bi-people me-2"></i>
+                Participants
+                </Button>
+              <Button variant="light" active className="mb-1 text-start bg-dark-subtle border-0">
+                <i className="bi bi-table me-2"></i>
+                Results
+                </Button>
+              <Button variant="light" className="mb-1 text-start border-0" onClick={onEditTestClick}>
+                <i className="bi bi-pencil-square me-2"></i>
+                Edit questions...
+                </Button>
+            </div>
+          </RightSideMenu>
+        </Col>
+      </Row>
     </>
   );
 };
--- src/constants/AnswerGridColumns.tsx
+++ src/constants/AnswerGridColumns.tsx
@@ -1,13 +1,152 @@
+import { Col, Form, Image, Row, Spinner } from "react-bootstrap";
+import profilePic from "../images/blank-profile-picture.jpg";
+
 export const AnswerGridCol = [
   {
-    field: "status",
-    flex: 1,
-    headerName: "Status",
+    minWidth: 200,
+    sortable: true,
+    unSortIcon: true,
+    field: "student",
+    flex: 2,
+    autoWidth: true,
+    autoHeight: true,
+    headerName: "Student",
+    getQuickFilterText: (params: any) => {
+      console.log(params);
+      return (params.value.name + "\n" + params.value.customId);
+    },
+    cellRenderer: (params: any) => (
+      <div className="d-flex my-1">
+        <div className="d-flex flex-column">
+          <Image className="profile-image my-1 ms-0 me-2" src={profilePic} />
+        </div>
+        <div className="d-flex flex-column lh-base">
+            <div>{params.value.name}</div>
+            <div>{params.value.customId}</div>
+        </div>
+      </div>
+    ),
+    // pinned: true,
   },
-  { field: "studentId", flex: 2, headerName: "Student Id" },
-  { field: "studentName", flex: 2, headerName: "Student Name" },
-  { field: "date", flex: 2, headerName: "Date" },
-  { field: "duration", flex: 1, headerName: "Duration" },
-  { field: "mark", flex: 1, headerName: "Mark" },
-  //{ field: "", flex: 0.1, headerName: "", cellRenderer: BtnCellRenderer},
+  {
+    minWidth: 175,
+    field: "startEnd",
+    sortable: true,
+    unSortIcon: true,
+    flex: 1.5,
+    autoHeight: true,
+    headerName: "Timeline",
+    getQuickFilterText: (params: any) => {
+      return (params.value.start + " - " + params.value.end);
+    },
+    cellRenderer: (params: any) => (
+      <div className="d-flex flex-column lh-base">
+            <div>{params.value.start} -</div>
+            <div>{params.value.end}</div>
+      </div>
+    ),
+  },
+  { 
+    minWidth: 100,
+    field: "grade", 
+    sortable: true,
+    unSortIcon: true,
+    flex: 1, 
+    headerName: "Grade",
+    cellRenderer: (params: any) => (
+      params.value && (
+      <div>
+        <b>{params.value.grade}</b> ({params.value.percent}%)
+        </div>)
+    ),
+  },
+  { 
+    minWidth: 100,
+    field: "isShown",
+    sortable: true,
+    unSortIcon: true,
+    flex: 1, 
+    headerName: "Shown?",
+    getQuickFilterText: (params: any) => {
+      return (params.value ? "Yes" : "No");
+    },
+    cellRenderer: (params: any) => (
+      <Form.Select size="sm" className="mt-2">
+        <option selected={!params.value} value={0}>No</option>
+        <option selected={params.value} value={1}>Yes</option>
+    </Form.Select>
+    ),
+  },
+  { 
+    minWidth: 125,
+    field: "status", 
+    sortable: true,
+    unSortIcon: true,
+    flex: 1, 
+    headerName: "Status", 
+    getQuickFilterText: (params: any) => {
+      switch (params.value){
+        case 0:
+          return "In progress";
+        case 1:
+          return "Submitted";
+        case 2:
+          return "Autochecking...";
+        case 3:
+          return "Passed";
+        case 4:
+          return "Failed";
+        default:
+          return "?";
+      };
+    },
+    cellRenderer: (params: any) => 
+      {
+        switch (params.value) {
+          case 0:
+            return (
+            <div className="text-secondary">
+              <i className="bi bi-circle ms-1 me-2"></i>
+              In progress
+              </div>);
+          case 1:
+            return (
+            <div className="text-primary">
+              <i className="bi bi-exclamation-circle-fill ms-1 me-2"></i>
+              Submitted
+              </div>
+            );
+          case 2:
+            return (
+            <div className="text-secondary">
+              <Spinner animation="border" size="sm" className="me-2" />
+              Checking...
+              </div>
+            );
+          case 3:
+              return (
+              <div className="text-success">
+                <i className="bi bi-check-circle-fill ms-1 me-2"></i>
+                Passed
+                </div>
+                );
+          case 4:
+              return (
+              <div className="text-danger">
+                <i className="bi bi-x-circle-fill ms-1 me-2"></i>
+                Failed
+                </div>
+                );
+          default:
+            return (<div className="text-danger">?</div>);
+      }}
+    
+    },
+    {
+      minWidth: 50,
+      headerCheckboxSelection: true,
+      headerCheckboxSelectionFilteredOnly: true,
+      checkboxSelection: true,
+      flex: 0.5,
+    },
 ];
