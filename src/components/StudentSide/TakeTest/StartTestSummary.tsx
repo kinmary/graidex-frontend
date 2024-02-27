fbfb1d2c2aded6886aa94216efe7da5de3544a70
BarysShauchuk <borshevchukn@gmail.com>
2024-02-27 18:19:32 +0200

    Change UI of StartTestSummary page

--- src/components/StudentSide/TakeTest/StartTestSummary.tsx
+++ src/components/StudentSide/TakeTest/StartTestSummary.tsx
@@ -4,6 +4,9 @@ import {
   Form,
   Breadcrumb,
   Alert,
+  Col,
+  Row,
+  ListGroup,
 } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../app/store";
@@ -127,28 +130,70 @@ const StartTestSummary = () => {
             </Breadcrumb.Item>
           </Breadcrumb>
           {currentTestDraft !== undefined && (
-            <div>
-              <p>{currentTestDraft.description}</p>
-
-              <div className="text-center">
-                <h6 className="mt-5">
-                  Grade to pass the test: {currentTestDraft.gradeToPass}
-                </h6>
-                <h6>Start date and time: {startDate.toLocaleString()} </h6>
-                <h6>End date and time: {endDate.toLocaleString()} </h6>
-                <h6>Time Limit: {currentTestDraft.timeLimit} </h6>
+            <Row className="mt-2 d-flex justify-content-center">
+              {currentTestDraft.description ? (
+              <Col>
+                <p className="bg-white border border-light-subtle rounded p-2" style={{minHeight: '100%'}}>{currentTestDraft.description}</p>
+              </Col>
+              ):
+              <div className="mt-5"></div>
+              }
+              <Col className="d-flex flex-column gap-3 text-center"
+                   style={{maxWidth: '50%'}}>
+              <ListGroup className="user-select-none">
+                <ListGroup.Item action variant="light" as='div' className="d-flex align-items-center">
+                  <i className="bi bi-calendar-check me-3 fs-5"></i>
+                  Start date and time
+                  <div className="flex-fill"></div>
+                  <strong>{startDate.toLocaleString().slice(0, -3)}</strong>
+                </ListGroup.Item>
+                <ListGroup.Item action variant="light" as='div' className="d-flex align-items-center">
+                  <i className="bi bi-calendar-x me-3 fs-5"></i>
+                  End date and time
+                  <div className="flex-fill"></div>
+                  <strong>{endDate.toLocaleString().slice(0, -3)}</strong>
+                </ListGroup.Item>
+                <ListGroup.Item action variant="light" as='div' className="d-flex align-items-center">
+                  <i className="bi bi-stopwatch me-3 fs-5"></i>
+                  Time limit
+                  <div className="flex-fill"></div>
+                  <strong>{currentTestDraft.timeLimit}</strong>
+                </ListGroup.Item>
+                <ListGroup.Item action variant="light" as='div' className="d-flex align-items-center">
+                  {/* TODO: Change to <i className="bi bi-crosshair me-3 fs-5"></i> */}
+                  <i className="bi bi-bullseye me-3 fs-5"></i>
+                  Grade to pass the test
+                  <div className="flex-fill"></div>
+                  <strong>{currentTestDraft.gradeToPass}</strong>
+                </ListGroup.Item>
+              </ListGroup>
+                
                 {startDate.getTime() > new Date().getTime() && (
-                  <Alert variant="warning">This test has not started yet</Alert>
+                  <Alert variant="warning" className="mb-0 d-flex align-items-center">
+                    This test has not started yet
+                    <Button className="ms-auto" variant="outline-warning" size="sm"
+                     onClick={() => window.location.reload()}>
+                      <i className="bi bi-arrow-repeat me-2"></i>
+                      Refresh page
+                     </Button>
+                  </Alert>
                 )}
                 {endDate.getTime() < new Date().getTime() && (
-                  <Alert variant="danger">This test has already ended</Alert>
+                  <Alert variant="danger" className="mb-0 d-flex align-items-center">
+                    This test has already ended
+                    <Button className="ms-auto" variant="outline-danger" size="sm"
+                          href="mailto:example@email.com?subject=Missed test&body=Hello%2C%0A%0ALooks%20like%20I%20missed%20a%20test%2C%20can%20we%20agree%20on%20a%20retake%20date%3F%0A">
+                      <i className="bi bi-envelope me-2"></i>
+                      Email teacher
+                    </Button>
+                  </Alert>
                 )}
                 {attemptsInfo.currentTestResultId !== undefined &&
                 attemptsInfo.currentTestResultId !== null &&
                 attemptsInfo.currentTestResultId > 0 ? (
                   <Button
                     variant="outline-primary"
-                    style={{ marginTop: 10, width: "100%" }}
+                    className="mx-5"
                     onClick={onContinueClick}
                     disabled={
                       startDate.getTime() > new Date().getTime() ||
@@ -160,7 +205,7 @@ const StartTestSummary = () => {
                 ) : attemptsInfo.numberOfAvailableTestAttempts > 0 ? (
                   <Button
                     variant="primary"
-                    style={{ marginTop: 10, width: "100%" }}
+                    className="mx-5"
                     onClick={onStartClick}
                     disabled={
                       startDate.getTime() > new Date().getTime() ||
@@ -170,10 +215,10 @@ const StartTestSummary = () => {
                     Start test
                   </Button>
                 ) : (
-                  <Alert variant="secondary">No more available attempts</Alert>
+                  <Alert variant="secondary" className="mb-0">No more available attempts</Alert>
                 )}
-              </div>
-            </div>
+              </Col>
+            </Row>
           )}
         </Form>
       )}
